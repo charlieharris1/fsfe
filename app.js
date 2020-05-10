4caa13948807343c2d235bf7fcefb323b3f92628
charlieharris1 <ceharris92@gmail.com>
2020-05-10 14:13:17 +0100

    Add 418 path

--- app.js
+++ app.js
@@ -6,4 +6,9 @@ app.get('/', (req, res) => {
 	res.send('Hello world!');
 })
 
+app.get('/418', (req, res) => {
+	res.status(418);
+	res.set('X-Custom-Header', 'A custom header')
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
